78ae4e58b297d910a08017d56d58daef6185f116
Toshiaki Toyama <ttoyama@knownstylenolife.com>
2023-08-20 01:47:13 +0900

    Add pyinstrument profiler to set cpu time and wallclock time to server-timing header

--- llm-app/README.md
+++ llm-app/README.md
@@ -6,7 +6,7 @@
 poetry run uvicorn src.llm_app.api.main:app --host localhost --reload
 
 # run dredd
-poetry run dredd ./tests/dredd/openapi-predict.json http://127.0.0.1:8000 --server "uvicorn src.llm_app.api.main:app"
+poetry run dredd ./tests/dredd/openapi-predict.json http://127.0.0.1:8000 --server "uvicorn src.llm_app.api.main:app" --server-wait 13
 
 # run dredd with dredd.yml and debug level
 dredd ---config dredd.yml --level=debug
--- llm-app/src/llm_app/api/main.py
+++ llm-app/src/llm_app/api/main.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import time
+import json
+
+from pyinstrument import Profiler
+from pyinstrument.renderers import JSONRenderer
+
+from dataclasses import dataclass
 from typing import Any
+
+from devtools import debug
 
 from fastapi import Depends, FastAPI, Response, Query
 from pydantic import BaseModel, Field
@@ -20,23 +29,60 @@ class PredictOut(BaseModel):
     message: str
 
 
+@dataclass
+class ServerTimingEntry:
+    label: str = 'None'
+    desc: str = 'None'
+    dur: int = 0
+
+    def __str__(self):
+        return '{};dur={};desc="{}"'.format(self.label, self.dur, self.desc)
+
+
+@dataclass
+class ServerTiming:
+    cpu_time: ServerTimingEntry
+    wall_clock_time: ServerTimingEntry
+
+    def __post_init__(self):
+        self.cpu_time = ServerTimingEntry(label='func_cpu_time', dur=self.cpu_time['dur'], desc="Func call cpu time")
+        self.wall_clcok_time = ServerTimingEntry(label='func_wall_clock_time', dur=self.wall_clock_time['dur'], desc="Func call wall clock time")
+
+    def __str__(self):
+        return ','.join([str(self.cpu_time), str(self.wall_clcok_time)])
+
+
 @app.get("/predict", response_model=PredictOut)
-async def predict(response: Response, query: PredictIn = Depends()) -> Any:
+def predict(response: Response, query: PredictIn = Depends()) -> Any:
+
+    profiler = Profiler()
+    profiler.start()
+
+    time.sleep(1)
+
+    profiler.stop()
+    jd = json.loads(profiler.output(JSONRenderer(show_all=False, timeline=False)))
+    # debug(jd)
+
     # performace measure with Server-Timing
     #  Server Timing, https://w3c.github.io/server-timing/#examples
-    response.headers["Server-Timing"] = ('sql-1;desc="MySQL lookup Server";dur=100, '
-                                         'sql-2;dur=900;desc="MySQL shard Server #1", '
-                                         'fs;dur=600;desc="FileSystem", '
-                                         'cache;dur=300;desc="Cache", '
-                                         'other;dur=200;desc="Database Write", '
-                                         'other;dur=110;desc="Database Read", '
-                                         'cpu;dur=1230;desc="Total CPU"')
 
-    # response.headers["Server-Timing"] = ('miss, db;dur=53, app;dur=47.4, '
-    #                                      'customView, dc;desc=atl, '
-    #                                      'cache;desc="Cache Read";dur=23.2, '
-    #                                      'Prerender;dur=1;desc="Headless render time (ms)", '
-    #                                      'total;dur=124.6')
+    st = {'wall_clock_time':{'dur':jd['duration'] * 1000},
+          'cpu_time':{'dur':jd['cpu_time'] * 1000}}
+    server_timing = ServerTiming(**st)
+    response.headers["Server-Timing"] = str(server_timing)
+
+    # wall_clock_time = 'func_cpu_time;dur={};desc="Func Call cpu time",'.format(jd['duration'])
+    # cpu_time = 'func_wall_clock_timeu;dur={};desc="Func call wall clock time",'.format(jd['cpu_time'])
+    # response.headers["Server-Timing"] = (cpu_time + wall_clock_time)
+
+    # response.headers["Server-Timing"] = ('sql-1;desc="MySQL lookup Server";dur=100, '
+    #                                      'sql-2;dur=900;desc="MySQL shard Server #1", '
+    #                                      'fs;dur=600;desc="FileSystem", '
+    #                                      'cache;dur=300;desc="Cache", '
+    #                                      'other;dur=200;desc="Database Write", '
+    #                                      'other;dur=110;desc="Database Read", '
+    #                                      'cpu;dur=1230;desc="Total CPU"')
 
     return {"message": f"Predict {query.name}!"}
 
